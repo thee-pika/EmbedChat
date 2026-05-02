a32fc59ce88ba777899f955c3300d675d4875415
Mogili Deepika <mogilideepika218@gmail.com>
2026-05-02 18:39:35 +0530

    made changes for production

--- public/chatBot.js
+++ public/chatBot.js
@@ -1,5 +1,5 @@
 (function () {
-  const api_Url = "http://localhost:3000/api/chat";
+  const api_Url = `${process.env.NEXT_PUBLIC_APP_URL}/api/chat`;
 
   const scriptTag = document.currentScript;
 
